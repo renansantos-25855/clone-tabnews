362efebf8bfa8c123725c3abcf1bf55780f36669
renansantos-25855 <73499338+renansantos-25855@users.noreply.github.com>
2023-09-11 10:54:21 +0000

    adiciona arquivo `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Oi minha vida, você é linda, perfeita, e eu te amo demais!! </h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentacao errada");
-}
-
 export default Home;
