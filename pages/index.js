52229039d5c46119ae97a9672886cd1508197bae
renansantos-25855 <73499338+renansantos-25855@users.noreply.github.com>
2023-09-14 11:01:11 +0000

    alterando texto principal do site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
 function Home() {
-  return <h1>Oi minha vida, você é linda, perfeita, e eu te amo demais!! </h1>;
+  return (
+    <div>
+      <h1>
+        Olá minha irmã ! Existem momentos da nossa vida que são mesmo confusos e
+        nos sentimos perdidos.
+      </h1>
+      <h2>
+        Mas não desiste, tá?! São nesses momentos que as decisões importantes
+        são tomadas e que temos
+      </h2>
+      <h2>a oportunidade de mudar o rumo de nossas vidas.</h2>
+      <h1>
+        E por fim, te convido a passar por aqui de vez em quando pra você ver a
+        evolução deste site!! Vai ser massa demais...
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
